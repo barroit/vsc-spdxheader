72525b45e50c9a901a81b82e7fdef8d3f7379914
Jiamu Sun <39@barroit.sh>
2026-04-07 17:36:09 +0900

    lib: drop unused files
    Signed-off-by: Jiamu Sun <39@barroit.sh>

--- cmd/add_cached.js
+++ cmd/add_cached.js
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-3.0-or-later */
 /*
  * Copyright 2025 Jiamu Sun <barroit@linux.com>
+ * Copyright 2026 Jiamu Sun <39@barroit.sh>
  */
 
 import { vsc_exec_cmd } from '../lib/vsc.js'
